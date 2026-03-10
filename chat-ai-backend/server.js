3c3393f52a63e1fa7e2729516ef985975d9ce090
manisha <banerjeemanisha763@gmail.com>
2026-03-10 20:13:03 +0530

    push

--- chat-ai-backend/server.js
+++ chat-ai-backend/server.js
@@ -21,8 +21,8 @@ app.post("/api/generate", async (req, res) => {
   }
 
   try {
-   const response = await axios.post(
-`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${process.env.GEMINI_API_KEY}`,
+ const response = await axios.post(
+`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
 {
   contents: [
     {
